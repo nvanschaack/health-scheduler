4cf039612e5e37830be62a2cf628b8227e37da35
nvanschaack <nellvanschaack@gmail.com>
2024-10-24 16:07:53 -0500

    first commit. created a sql server folder with server boilerplate code and file structure + client folder

---
+++ sql-server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+// const routes = require('./routes');
+const path = require('path')
+
+//define PORT
+const PORT = 3001;
+
+//define middleware
+app.use(express.json());  //means everything coming into or going out of server needs to be in json format
+app.use(express.urlencoded({ extended: true }));  //used to access the req.body
+// app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')))
+};
+
+app.listen(PORT, () => {
+    console.log('server is running on port 3001');
+});
