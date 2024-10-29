23d9fa98a69a480af9e3d50921f8f6c660f66be3
nvanschaack <nellvanschaack@gmail.com>
2024-10-29 18:02:36 -0500

    added medicalHxControllers

--- sql-server/controllers/medicalHxControllers.js
+++ sql-server/controllers/medicalHxControllers.js
@@ -1,8 +1,29 @@
 const db = require('../config/connection');
 
 module.exports = {
-    //getMedicalHx
-    //addMedicalHx
-    
+    getMedicalHx(req,res){
+        const sql = `SELECT * FROM medical_hx WHERE patientId = ${req.body.patientId}`;
+
+        db.query(sql, (err, data)=> {
+            if (err) {
+                res.status(500).json(err)
+            }
+            if (data.length === 0) {
+                res.status(400).json('patient id not found')
+            }
+            res.status(200).json(data)
+        })
+    },
+    addMedicalHx(req,res){
+        const sql = `INSERT INTO medical_hx (diagnosis, dateOfDiagnosis, tx, courseOfTx, patientId) VALUES ('${req.body.diagnosis}', '${req.body.dateOfDiagnosis}', '${req.body.tx}', '${req.body.courseOfTx}', '${req.body.patientId}')`;
+
+        db.query(sql, (err, data)=> {
+            if (err) {
+                res.status(500).json(err)
+            }
+            res.status(200).json(data)
+        })
+    }
+
     //future development - updateMedicalHx
 }
