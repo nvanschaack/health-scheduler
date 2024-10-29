ad175f0bd018b715c9b5278750bee7239bf07199
nvanschaack <nellvanschaack@gmail.com>
2024-10-29 12:09:21 -0500

    added the rest of usercontrollers and auth.js for token signing and extracting

--- sql-server/controllers/userControllers.js
+++ sql-server/controllers/userControllers.js
@@ -1,12 +1,13 @@
 const db = require('../config/connection');
 const bcrypt = require('bcrypt');
+const auth = require('../utils/auth');
 
 module.exports = {
-    //addAPatient
-    //when we add a patient we dont want to make their password for them though???
+    //when the user registers their account
     async addAPatient(req, res) {
-        //need to hash password
-        const sql = `INSERT INTO user (role, firstName, lastName, age, username, password) VALUES ('${req.body.role}','${req.body.firstName}','${req.body.lastName}','${req.body.age}','${req.body.password}',)`;
+        const hashedPw = await bcrypt.hash(req.body.password, 10);
+
+        const sql = `INSERT INTO user (firstName, lastName, age, username, password) VALUES ('${req.body.firstName}','${req.body.lastName}','${req.body.age}','${req.body.username}','${hashedPw}',)`;
 
         db.query(sql, (err, data) => {
             if (err) {
@@ -15,9 +16,11 @@ module.exports = {
             res.json('patient created')
         })
     },
-    //addAProvider
+    //admin adds a provider to the system
     async addAProvider(req, res) {
-        const sql = `INSERT INTO user (role, firstName, lastName, age, username, password) VALUES ('${req.body.role}','${req.body.firstName}','${req.body.lastName}','${req.body.age}','${req.body.password}',)`;
+        const hashedPw = await bcrypt.hash(req.body.password, 10);
+
+        const sql = `INSERT INTO user (role, firstName, lastName, age, username, password) VALUES ('${req.body.role}','${req.body.firstName}','${req.body.lastName}','${req.body.age}','${req.body.username}','${hashedPw}',)`;
 
         db.query(sql, (err, data) => {
             if (err) {
@@ -26,18 +29,79 @@ module.exports = {
             res.json('provider created')
         })
     },
-    //login
-    async login(req,res) {
+    login(req, res) {
         const sql = `SELECT * FROM user WHERE username = '${req.body.username}'`
-        db.query(sql, (err, data)=>{
+        db.query(sql, (err, data) => {
+            if (data.length === 0) {
+                return res.status(400).json('username is not in the database')
+            }
+            console.log("data:", data);
             
-        })
-        
-    }
-    //deleteAUser
-    //seeAllPatients
-    //seeAllProviders
-    //seeAllUsers
+            const user = data[0];
 
+            const checkClientPw = bcrypt.compare(req.body.password, user.password);
+
+            if (!checkClientPw) {
+                res.status(400).json('password does not match password in database')
+            }
+            const token = auth.signToken(user)
+
+            res.status(200).json({token})
+        })
+
+    },
+    findOneUser(req,res){
+        const sql = `SELECT * FROM user WHERE id = ${req.user.id}`
+
+        db.query(sql, (err,data)=>{
+            if (err) {
+                res.status(500).json(err)
+            }
+            if (data.length === 0) {
+                res.status(400).json('user not found')
+            }
+
+            res.status(200).json(data)
+        })
+    },
+    deleteAUser(req, res) {
+        const sql = `DELETE FROM user WHERE id = ${req.params.userId}`
+        db.query(sql, (err, data) => {
+            if (err) {
+                res.status(500).json(err)
+            }
+
+            if (data.length === 0) {
+                res.status(400).json('user not found')
+            }
+
+            res.status(200).json('user deleted')
+        })
+    },
+    //this fxn can return 3 different pieces of data using req.query
+    // /api/user?role=all
+    seeAllByRole(req, res) {
+
+        let sql;
+        if (req.query.role === 'patient') {
+
+            sql = `SELECT * FROM user WHERE role = '${req.query.role}'`
+        }
+        else if (req.query.role === 'provider') {
+            sql = `SELECT * FROM user WHERE role = '${req.query.role}'`
+        }
+        else {
+            sql = `SELECT * FROM user`
+        }
+
+        db.query(sql, (err, data) => {
+            if (err) {
+                res.status(500).json(err)
+            }
+
+            res.status(200).json(data)
+        })
+    },
+  
     //future development - editProfile
 }
---
+++ sql-server/utils/auth.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+    authMiddleware: function (req, res, next) {
+        // allows token to be sent via  req.query or headers
+        // the value of "token" is dependent on if the query or headers exist
+        let token = req.query.token || req.headers.authorization;
+    
+        // ["Bearer", "<tokenvalue>"]
+        if (req.headers.authorization) {
+          //extracting the token from headers
+          token = token.split(' ').pop().trim();
+        }
+    
+        if (!token) {
+          //if token does not exist, send this message
+          return res.status(400).json({ message: 'You have no token!' });
+        }
+    
+        // verify token and get user data out of it
+        try {
+          // verifying that the token's secret and expiration match the secret and expiration provided by the code
+          //extract the existing property from the token called data
+          const { data } = jwt.verify(token, secret, { maxAge: expiration });
+          // assigning data to a new property in the request called user
+          //req.user is an object with "username" and "id" as keys
+          req.user = data;
+        } catch {
+          console.log('Invalid token');
+          return res.status(400).json({ message: 'invalid token!' });
+        }
+    
+        // send to next endpoint
+        next();
+      },
+      //you can store whatever you want in the signToken parameter, doesn't need to just be username and id
+      //although "user" i spassed as the parameter for signToken in usercontrollers, here we are using object deconstruction to just get access to the username and id properties
+      signToken: function ({ username, id }) {
+        const payload = { username, id };
+    
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+      },
+}
