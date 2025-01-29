13ef32513f2d9d0c442df20cb32bfe90a4f21e88
nvanschaack <nellvanschaack@gmail.com>
2025-01-28 19:12:43 -0600

    started crreated function on DeleteAvailabilityButton called delAvail to delete providers availability when clicked

--- client/src/components/provider/DeleteAvailability.jsx
+++ client/src/components/provider/DeleteAvailability.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { TrashIcon } from "@heroicons/react/24/solid";
+import auth from "../../utils/auth";
 
 //API Imports
+import { deleteAvailability } from "../../utils/providerApi";
 
 //deconstructing availabilityId from props
-export default function DeleteAvailabilityButton({availabilityId}) {
-    //add functionality - needs to receive availabilityID
+export default function DeleteAvailabilityButton({ availabilityId }) {
+  //GLOBAL TOKEN
+  const token = auth.retrieveTokenFromLocalStorage();
+
+  //STOPPED HERE
+  const delAvail = async () => {
+    try {
+      const del = await deleteAvailability(token, availabilityId);
+      //do i need this since I'm technically just deleting data?
+      const response = await del.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <TrashIcon className="size-6 text-red-500" onClick={()=>console.log(availabilityId)
-      }/>
+      <TrashIcon
+        className="size-6 text-red-500"
+        onClick={() => console.log(availabilityId)}
+      />
     </div>
   );
 }
